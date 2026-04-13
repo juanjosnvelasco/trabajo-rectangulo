64bc027d2c4c65f10e3c8c98cb06fa911abbba73
juanc <juan_josn.velasco@uao.edu.co>
2026-04-13 11:59:33 -0500

    creando modelo MVC

---
+++ src/main/java/com/mycompany/rectangulo/Rectangulo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rectangulo;
+
+/**
+ *
+ * @author juanc
+ */
+public class Rectangulo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
